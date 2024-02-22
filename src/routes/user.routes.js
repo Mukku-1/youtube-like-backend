5886dfc9e594d1c864c7451260351409d6d919a9
Mukku-1 <mkrrm27@outlook.com>
2024-02-22 22:23:20 +0530

    envirment varaible change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshedAccessToken,
   registerUser,
+  updataUserAvatar,
+  updataUserCoverImage,
+  updateUserDetail,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -23,5 +28,15 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshedAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateUserDetail);
+
+router
+  .route("/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updataUserAvatar);
+router
+  .route("/cover-imager")
+  .patch(verifyJwt, upload.single("coverImage"), updataUserCoverImage);
 
 export default router;
